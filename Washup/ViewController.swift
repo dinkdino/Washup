256da833ef7684335d5bb368245cfdd06614575a
Hrishikesh Sawant <Dino@HrishikeshsiMac.dinklink>
2015-02-06 21:09:32 +0530

    Initial Commit

---
+++ Washup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Washup
+//
+//  Created by Hrishikesh Sawant on 06/02/15.
+//  Copyright (c) 2015 Hrishikesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
